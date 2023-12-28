9fef23a64dbf9e7fa8688a8d3a6f7fc62e92ca24
tawanrio <stawanrio@gmail.com>
2023-12-28 18:57:37 -0300

    select none

--- src/components/Banner/components/Arrow.jsx
+++ src/components/Banner/components/Arrow.jsx
@@ -23,15 +23,16 @@ export default function Arrow({controllerBanner}) {
       justify-center
       md:w-1/12
       w-2/12
+      select-none
       flex
       " role="button" tabindex="0" href="#" onClick={prevBanner}>
-      <svg className="md:w-24 w-8" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-linecap="round" stroke-linejoin="round" ><path d="m15 18-6-6 6-6"/></svg>
+      <svg className="md:w-24 w-8 select-none" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-linecap="round" stroke-linejoin="round" ><path d="m15 18-6-6 6-6"/></svg>
       </a>
       <a 
       style={{ color:banners.colors.controllers }}
       className="
       md:h-banner-home-h
-
+      select-none
       absolute 
       top-0 
       flex 
@@ -42,7 +43,7 @@ export default function Arrow({controllerBanner}) {
       h-52
       items-center
       " role="button" tabindex="0" href="#" onClick={nextBanner}>
-        <svg className="md:w-24 w-8" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-linecap="round" stroke-linejoin="round" ><path d="m9 18 6-6-6-6"/></svg>
+        <svg className="md:w-24 w-8 select-none" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-linecap="round" stroke-linejoin="round" ><path d="m9 18 6-6-6-6"/></svg>
       </a>
         </div>
   )
