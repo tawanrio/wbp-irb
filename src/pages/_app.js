4194f3247ef50405e89bc39343d05913195197f3
tawsouza <stawanrio@gmail.com>
2024-02-06 14:54:14 -0300

    font inter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,17 @@ import '@/styles/globals.css'
 // import '@/layouts/irb/Header/header.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import ContextPage from '@/context/pageData';
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 import '@/styles/root.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <ContextPage >
+      <main className={inter.className}>
       <Component {...pageProps} />
+      </main>
     </ContextPage>
   )
 }
