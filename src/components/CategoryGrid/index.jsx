0b47a5e3ec5fbcf5e3bb9106135409ac07d9e4bb
Tawan <stawanrio@gmail.com>
2024-07-01 15:30:48 -0300

    parceiros IRB

--- src/components/CategoryGrid/index.jsx
+++ src/components/CategoryGrid/index.jsx
@@ -5,7 +5,7 @@ const CategoryGrid = ({ categories, title }) => {
   return (
     <section className="relative flex flex-col items-center mt-14" id="blog-carousel">
     <div className="w-full relative md:max-w-7xl flex-col md:px-14 md:mb-0 my-4 px-6 flex max-w-lg">
-   <SectionTitle title={"Nossa Linha De Produtos"} line/>
+   <SectionTitle title={"Nossa Linha de Produtos"} line/>
     <div className="container mx-auto py-10 my-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {categories.slice(0, 6).map((category) => (
