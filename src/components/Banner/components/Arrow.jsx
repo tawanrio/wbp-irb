6de033ca2f01edad2de31dfdb0afbb5c7de91205
Tawan <stawanrio@gmail.com>
2024-07-04 03:15:57 -0300

    https

--- src/components/Banner/components/Arrow.jsx
+++ src/components/Banner/components/Arrow.jsx
@@ -26,7 +26,7 @@ export default function Arrow({controllerBanner}) {
       select-none
       flex
       " role="button" href="#" onClick={prevBanner}>
-      <svg className="md:w-24  w-8 select-none" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  ><path d="m15 18-6-6 6-6"/></svg>
+      <svg className="md:w-24  w-8 select-none" xmlns="https://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  ><path d="m15 18-6-6 6-6"/></svg>
       </a>
       <a 
       style={{ color:banners?.colors.controllers }}
@@ -44,7 +44,7 @@ export default function Arrow({controllerBanner}) {
       z-10
       items-center
       " role="button" href="#" onClick={nextBanner}>
-        <svg className="md:w-24 w-8  select-none" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  ><path d="m9 18 6-6-6-6"/></svg>
+        <svg className="md:w-24 w-8  select-none" xmlns="https://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor"  ><path d="m9 18 6-6-6-6"/></svg>
       </a>
         </div>
   )
