5697750f2700ff37bd5b33bd32a6f900bc96528f
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-07-23 13:20:55 -0300

    refactor: change company details image to default

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Image from 'next/image'
 import Link from 'next/link'
-import { formatPhoneNumber, formatToViewPhone } from '@/utils/functions'
+import { formatToViewPhone } from '@/utils/functions'
 
 export default function ButtonTell({ phone, whatsapp }) {
   const formatedWhatsappNumber = whatsapp && formatToViewPhone(whatsapp?.number)
@@ -16,12 +16,11 @@ export default function ButtonTell({ phone, whatsapp }) {
           href={actionPhone}
           target="_blank"
           style={{
-            width: phone?.layout?.width,
             height: phone?.layout?.height,
             backgroundColor: phone?.layout?.colors.bg,
             border: phone?.layout?.border,
           }}
-          className="flex flex-col justify-center rounded-2xl bg-slate-800 px-5 py-3"
+          className="flex flex-1 flex-col justify-center rounded-2xl bg-slate-800 px-5 py-3"
         >
           <div className="relative flex items-center">
             {phone?.icon && (
@@ -40,7 +39,7 @@ export default function ButtonTell({ phone, whatsapp }) {
                 color: phone?.layout?.colors.text,
                 fontWeight: phone?.layout?.weight,
               }}
-              className={`ml-2 text-center text-lg uppercase opacity-100 md:text-2xl ${phone?.number && 'group-hover:opacity-0'} duration-500 ${phone?.icon.url && 'ml-2'} `}
+              className={`ml-2 whitespace-nowrap text-center text-lg uppercase opacity-100 md:text-2xl ${phone?.number && 'group-hover:opacity-0'} duration-500 ${phone?.icon.url && 'ml-2'} `}
             >
               {formatedPhoneNumber}
             </span>
@@ -51,7 +50,7 @@ export default function ButtonTell({ phone, whatsapp }) {
                   fontSize: phone?.layout?.size,
                   fontWeight: phone?.layout?.weight,
                 }}
-                className={`absolute w-full scale-[.82] text-center text-lg uppercase opacity-0 duration-700 group-hover:opacity-100 md:text-2xl ${phone?.icon.url && 'ml-2'} `}
+                className={`absolute w-full scale-[.82] whitespace-nowrap text-center text-lg uppercase opacity-0 duration-700 group-hover:opacity-100 md:text-2xl ${phone?.icon.url && 'ml-2'} `}
               >
                 {formatedPhoneNumber}
               </span>
@@ -65,12 +64,11 @@ export default function ButtonTell({ phone, whatsapp }) {
           href={apiWhats}
           target="_blank"
           style={{
-            width: whatsapp?.layout?.width,
             height: whatsapp?.layout?.height,
             backgroundColor: '#72C971',
             border: '#ddd',
           }}
-          className="flex flex-col justify-center rounded-2xl bg-slate-800 px-5 py-3"
+          className="flex flex-1 flex-col justify-center rounded-2xl bg-slate-800 px-5 py-3"
         >
           <div className="relative flex items-center">
             {whatsapp?.icon && (
@@ -86,7 +84,7 @@ export default function ButtonTell({ phone, whatsapp }) {
             )}
             <span
               style={{ color: 'white', fontWeight: whatsapp?.layout?.weight }}
-              className={`ml-2 text-center text-lg uppercase opacity-100 md:text-2xl ${whatsapp?.number && 'group-hover:opacity-0'} duration-500 ${whatsapp?.icon.url && 'ml-2'} `}
+              className={`ml-2 whitespace-nowrap text-center text-lg uppercase opacity-100 md:text-2xl ${whatsapp?.number && 'group-hover:opacity-0'} duration-500 ${whatsapp?.icon.url && 'ml-2'} `}
             >
               {formatedWhatsappNumber}
             </span>
@@ -97,7 +95,7 @@ export default function ButtonTell({ phone, whatsapp }) {
                   fontSize: whatsapp?.layout?.size,
                   fontWeight: whatsapp?.layout?.weight,
                 }}
-                className={`absolute w-full scale-[.82] text-center text-lg uppercase opacity-0 duration-700 group-hover:opacity-100 md:text-2xl ${whatsapp?.icon.url && 'ml-2'} `}
+                className={`absolute w-full scale-[.82] whitespace-nowrap text-center text-lg uppercase opacity-0 duration-700 group-hover:opacity-100 md:text-2xl ${whatsapp?.icon.url && 'ml-2'} `}
               >
                 {formatedWhatsappNumber}
               </span>
--- src/components/IrbContact/index.jsx
+++ src/components/IrbContact/index.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Image from 'next/image'
-import SectionTitle from '../SectionTitle'
 import Description from '../Description'
 import Button from '../Button'
+import imgCallCenter from '../../../public/images/pages/contact/call-center.jpg'
 
 export default function ProductFaq({
   logo,
@@ -13,27 +11,22 @@ export default function ProductFaq({
   phone,
 }) {
   return (
-    <section className="flex flex-col items-center" id={`irb-contact_`}>
-      <div className="flex w-full max-w-7xl flex-col px-6 md:my-10 md:gap-10 md:px-14">
-        <div className="flex flex-col gap-10 md:flex-row">
-          <div className="w-12/12 flex flex-1 flex-col flex-wrap justify-center md:w-4/12">
-            <div className="relative hidden items-center md:block">
-              {/* <Image
-                                src={logo?.url}
-                                alt={logo?.alt}
-                                sizes="100vw"
-                                quality={80}
-                                fill
-                            /> */}
-              <img src={logo?.url} alt={logo.alt} className="w-full" />
-            </div>
-          </div>
-          <div className="w-12/12 md:w-7/12">
+    <section className="flex flex-col items-center" id="irb-contact_">
+      <div className="flex w-full max-w-7xl flex-col px-6 pt-6 md:gap-10 md:px-14 md:pb-10">
+        <div className="flex flex-col gap-10 lg:flex-row">
+          <figure className="mx-auto flex max-h-80 w-full max-w-[27.5rem] flex-col flex-wrap justify-center overflow-hidden rounded-lg md:w-[27.5rem]">
+            <img
+              src={imgCallCenter.src}
+              alt={logo.alt}
+              className="h-full w-full object-cover"
+            />
+          </figure>
+          <div className="w-full lg:w-7/12">
             <h2 className="mb-5 text-2xl font-bold uppercase text-[#666]">
               {title}
             </h2>
             <Description content={contentDescription} />
-            <div className="my-8 flex flex-col gap-8 md:flex-row">
+            <div className="my-8 flex flex-col flex-wrap gap-8 md:flex-row">
               {whatsapp && <Button whatsapp={whatsapp} />}
               {phone && <Button phone={phone} />}
             </div>
