8d601b380eb291091f5acc9cefabdd8a16893623
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-07-31 12:39:39 -0300

    fix: resolve bug with card links going to screen not found

--- src/components/Pages/Home/components/BlogCarousel/BlogCard/index.jsx
+++ src/components/Pages/Home/components/BlogCarousel/BlogCard/index.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Image from 'next/image'
+import Link from 'next/link'
 
-const BlogCard = ({ post }) => {
+export const BlogCard = ({ post }) => {
   const getUrlImage = (post) => {
     let urlImageDest = '/images/components/others/not-found.jpg'
     if (post?.yoast_head_json?.og_image) {
@@ -11,38 +10,37 @@ const BlogCard = ({ post }) => {
     return urlImageDest
   }
 
+  const formattedTitle = post?.title?.rendered
+    .split(' ')
+    .join('-')
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+
   return (
-    <div className="flex flex-col items-start justify-start overflow-hidden rounded-md bg-white shadow-md">
-      <div className="relative h-52 w-full">
+    <Link
+      href={`blog/${formattedTitle}`}
+      className="flex flex-col items-start justify-start overflow-hidden bg-white"
+    >
+      <figure className="relative h-52 w-full">
         <img
           src={getUrlImage(post)}
-          alt={post.image?.alt}
+          alt={post?.image?.alt}
           className="h-full w-full object-cover"
         />
-      </div>
+      </figure>
       <div className="flex flex-grow flex-col items-start justify-between p-4">
-        {' '}
-        {/* Usando flex-grow para garantir que este div ocupe o espaço restante */}
-        <div>
-          <h2 className="line-clamp-2 h-14 text-xl font-semibold text-[#222] md:h-16">
-            {post?.title.rendered}
-          </h2>
-          <p
-            className="line-clamp-5 text-[#222]"
-            dangerouslySetInnerHTML={{ __html: post?.excerpt.rendered }}
-          ></p>
-        </div>
-        <div>
-          <a
-            href={'blog/' + post.id}
-            className="inline-block rounded-md text-center font-semibold text-black transition-colors duration-500"
-          >
-            Saiba mais
-          </a>
-        </div>
+        <h2 className="line-clamp-2 h-14 text-xl font-semibold text-[#222]">
+          {post?.title?.rendered}
+        </h2>
+        <p
+          className="line-clamp-5 text-[#222]"
+          dangerouslySetInnerHTML={{ __html: post?.excerpt?.rendered }}
+        />
+        <p className="m-0 text-center font-semibold text-black transition-colors duration-500">
+          Saiba mais
+        </p>
       </div>
-    </div>
+    </Link>
   )
 }
-
-export default BlogCard
