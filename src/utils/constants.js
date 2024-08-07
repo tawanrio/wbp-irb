93593c2b57b0095004ac1df77996c99cefcc7200
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-08-06 21:30:44 -0300

    refactor: change the constant of place

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -31,3 +31,33 @@ export const EMAIL_RECIPIENTS = [
   'tawan.rio@webfoco.com',
   'rodrigo.silva@webfoco.com',
 ]
+
+export const PRODUCT_CATALOG_DETAILS = {
+  imageProduct: {
+    imageUrl: '/images/partners/bannerIRB.png',
+    alt: 'catalogo online',
+  },
+  title:
+    'Baixe agora mesmo o catálogo eletrônico e consulte facilmente toda nossa linha de produtos.',
+  subtitle: 'Acesse pelo computador ou celular',
+  button: [
+    {
+      title: '',
+      alt: 'appstore',
+      image: '/images/products/appstore.png',
+      link: 'https://apps.apple.com/br/app/irb-cat%C3%A1logo/id1169028455',
+    },
+    {
+      title: '',
+      alt: 'google-play',
+      image: '/images/products/google-play.png',
+      link: 'https://play.google.com/store/apps/details?id=br.com.ideia2001.CatalogoIRB',
+    },
+  ],
+
+  description: [
+    'Tenha acesso a nossa ampla gama de peças automotivas de alta qualidade para atender as necessidades do seu veículo.',
+    'Encontre tudo o que você precisa para manter o seu carro funcionando perfeitamente: rolamentos de roda, cubos de roda, trizetas, radiadores, intercoolers e eletroventiladores.',
+    'Baixe nosso catálogo hoje mesmo e descubra as melhores soluções para a manutenção e reparo do seu veículo!',
+  ],
+}
