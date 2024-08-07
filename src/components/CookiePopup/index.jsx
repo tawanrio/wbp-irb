543e935e3be68e3d8ccf1fc31c9422de7083963e
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-08-07 12:27:18 -0300

    feat: add cookie components

--- src/components/CookiePopup/index.jsx
+++ src/components/CookiePopup/index.jsx
@@ -1,66 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { PrivacyNotice } from './PrivacyNotice'
+import { ImportantNotice } from './ImportantNotice'
 
-const CookiePopup = () => {
-  const [showPopup, setShowPopup] = useState(false)
+export const CookiePopup = () => {
+  const [showPrivacyNotice, setShowPrivacyNotice] = useState(false)
+  const [showImportantNotice, setShowImportantNotice] = useState(false)
 
   useEffect(() => {
-    // Verifica se o cookie "cookieAccepted" existe
     const cookieAccepted = localStorage.getItem('cookieAccepted')
-    if (!cookieAccepted) {
-      // Se o cookie não existe, mostra o popup
-      setShowPopup(true)
+    const noticeAccepted = localStorage.getItem('noticeAccepted')
+
+    if (!noticeAccepted) {
+      setShowImportantNotice(true)
+    } else if (!cookieAccepted) {
+      setShowPrivacyNotice(true)
     }
   }, [])
 
-  const acceptCookies = () => {
-    // Define o cookie "cookieAccepted" quando o usuário aceita os cookies
+  const handleImportantNoticeClose = () => {
+    localStorage.setItem('noticeAccepted', 'true')
+    setShowImportantNotice(false)
+    setShowPrivacyNotice(true)
+  }
+
+  const handlePrivacyNoticeClose = () => {
     localStorage.setItem('cookieAccepted', 'true')
-    setShowPopup(false)
+    setShowPrivacyNotice(false)
   }
 
   return (
     <>
-      {showPopup && (
-        <div className="cookie-popup">
-          <div className="cookie-popup-content flex flex-col items-center gap-5">
-            <p>
-              Este site utiliza cookies para oferecer uma melhor experiência de
-              navegação. Ao continuar, você concorda com o uso de cookies e
-              aceita o armazenamento dos seus dados de acordo com a LGPD (Lei
-              Geral de Proteção de Dados).
-            </p>
-            <button className="accept-btn w-1/2" onClick={acceptCookies}>
-              Aceitar
-            </button>
-          </div>
-        </div>
+      {showImportantNotice && (
+        <ImportantNotice onClose={handleImportantNoticeClose} />
       )}
-      <style jsx>{`
-        .cookie-popup {
-          position: fixed;
-          bottom: 80px;
-          left: 50%;
-          transform: translateX(-50%);
-          background-color: #f0f0f0;
-          padding: 20px;
-          border-radius: 5px;
-          box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-          z-index: 9999;
-        }
-        .cookie-popup-content {
-          text-align: center;
-        }
-        .accept-btn {
-          background-color: rgb(34, 50, 110);
-          color: white;
-          border: none;
-          padding: 8px 16px;
-          border-radius: 4px;
-          cursor: pointer;
-        }
-      `}</style>
+      {!showImportantNotice && showPrivacyNotice && (
+        <PrivacyNotice onClose={handlePrivacyNoticeClose} />
+      )}
     </>
   )
 }
-
-export default CookiePopup
