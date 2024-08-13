0f907f036c0d9100259025bba055c5ce0d2c58a7
Tawan <stawanrio@gmail.com>
2024-08-13 17:41:42 -0300

    merge main

--- src/components/Pages/Singlepost/index.jsx
+++ src/components/Pages/Singlepost/index.jsx
@@ -1,11 +1,4 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-<<<<<<< HEAD
-import Templates from '@/components/Templates'
-import Banner from '@/components/Banner/index'
-import BreadCrumb from '@/components/BreadCrumb'
-
-=======
->>>>>>> cebbf8383e6ea4de98fda536d1e7e312aed202e5
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -14,8 +7,8 @@ import Banner from '@/components/Banner/index'
 import BreadCrumb from '@/components/BreadCrumb'
 
 export default function singlePost({ content }) {
-  const { title, metaTitle, metaDescription, contentDescription, banners } =
-    content?.page || {}
+  const { banners } = content?.page || {}
+
   const { posts } = content || {}
   const [sanitizedContent, setSanitizedContent] = useState('')
 
@@ -28,17 +21,8 @@ export default function singlePost({ content }) {
   return (
     <>
       <Head>
-<<<<<<< HEAD
-        <title>{metaTitle || title}</title>
-        <meta
-          name="description"
-          content={metaDescription || contentDescription}
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-=======
         <title>{posts.metaTitle}</title>
         <meta name="description" content={posts.metaDescription} />
->>>>>>> cebbf8383e6ea4de98fda536d1e7e312aed202e5
       </Head>
       <Templates
         template={content?.template}
@@ -47,24 +31,12 @@ export default function singlePost({ content }) {
       >
         <Banner banners={banners} />
         <BreadCrumb />
-<<<<<<< HEAD
-        <div className="mx-3.5 max-w-7xl py-8 md:mx-auto">
-=======
         <div className="max-w-7xl px-3.5 py-8 md:mx-auto">
->>>>>>> cebbf8383e6ea4de98fda536d1e7e312aed202e5
           <h1 className="mb-4 text-3xl font-bold">{posts?.title}</h1>
           <p className="mb-4 text-gray-600">
             {posts?._createdAt.split('T')[0]}
           </p>
           <div
-<<<<<<< HEAD
-            className="prose mb-8 overflow-hidden"
-            dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-          ></div>
-          <Link href="/blog" className="text-blue-600 hover:underline">
-            Voltar para Blog
-          </Link>
-=======
             className="prose overflow-hidden"
             dangerouslySetInnerHTML={{ __html: sanitizedContent }}
           ></div>
@@ -73,7 +45,6 @@ export default function singlePost({ content }) {
               Voltar para Blog
             </Link>
           </div>
->>>>>>> cebbf8383e6ea4de98fda536d1e7e312aed202e5
         </div>
       </Templates>
     </>
