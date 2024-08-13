a0cd8d912f9e7886ebc9d98983926026e0616208
Tawan <stawanrio@gmail.com>
2024-08-13 17:47:21 -0300

    style: popup notice

--- src/components/CookiePopup/ImportantNotice/index.jsx
+++ src/components/CookiePopup/ImportantNotice/index.jsx
@@ -8,7 +8,7 @@ export const ImportantNotice = ({ onClose }) => {
         <img
           src={ImportantNoticeImg.src}
           alt="Comunicado Importante"
-          className="h-full !max-h-[46.875rem] max-w-full"
+          className="h-full !max-h-[36rem] max-w-full"
         />
         <button
           onClick={onClose}
