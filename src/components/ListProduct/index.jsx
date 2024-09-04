ed0220f662480f2e7c38d755274ae88bfee73236
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-09-04 10:19:51 -0300

    style: add focus to buttons

--- src/components/ListProduct/index.jsx
+++ src/components/ListProduct/index.jsx
@@ -1,5 +1,6 @@
 import { cn } from '@/utils/cn'
 import { Product } from '../Product'
+import { LinkRed } from '../LinkRed'
 
 export const ListProduct = ({ categories }) => {
   const totalItems = categories.slice(0, 3).length
@@ -7,9 +8,12 @@ export const ListProduct = ({ categories }) => {
   return (
     <section
       id="blog-carousel"
-      className="relative mx-auto mt-14 flex w-full max-w-lg flex-col px-6 md:mb-0 md:max-w-[1600px] md:px-14"
+      className="relative mx-auto mt-28 flex w-full max-w-lg flex-col px-6 md:mb-0 md:max-w-[1600px] md:px-14"
     >
-      <ul className="container mx-auto my-5 flex py-10">
+      <LinkRed href="/" className="mx-auto w-full max-w-[281px]">
+        Nossos Produtos
+      </LinkRed>
+      <ul className="container mx-auto flex py-10">
         {categories.slice(0, 3).map((category, index) => (
           <Product
             key={category._id}
