6f088c7beb45f0d1fb56f496941aadd13e9f6138
Tawan <stawanrio@gmail.com>
2024-10-01 14:06:27 -0300

    feat: nofollow

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,41 +16,30 @@ export default function Document() {
           rel="stylesheet"
         ></link>
         <link rel="icon" href="/favicon.ico" />
-        {/* Search Console */}
-        <meta
-          name="google-site-verification"
-          content="NFE6O1WGcagwk4CALA-C6sP-QprTodIM_d8eOryFcU0"
-        />
-        {/* End Search Console */}
 
-        {/*  Google Tag Manager  */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {process.env.ENVIRONMENT === 'prod' ? (
+          <>
+            {' '}
+            {/* Search Console */}
+            <meta
+              name="google-site-verification"
+              content="NFE6O1WGcagwk4CALA-C6sP-QprTodIM_d8eOryFcU0"
+            />
+            {/* End Search Console */}
+            {/*  Google Tag Manager  */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-M4X5SDPW');`,
-          }}
-        />
-        {/* End Google Tag Manager  */}
-        {/*  Matomo  */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//irbauto.com.br:8099/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();`,
-          }}
-        />
-        {/* End Matomo  */}
+              }}
+            />
+          </>
+        ) : (
+          <meta name="robots" content="noindex, nofollow" />
+        )}
       </Head>
       <body>
         <noscript
