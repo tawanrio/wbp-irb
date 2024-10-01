9cb86814740252f11a69ec7466bbe176a884906c
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-10-01 14:43:55 -0300

    feat: add canonical

--- src/components/Pages/Parceiro/index.jsx
+++ src/components/Pages/Parceiro/index.jsx
@@ -1,37 +1,26 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Template / Layout
-import Head from 'next/head'
 import Templates from '@/components/Templates'
 
 // Components
 import BreadCrumb from '@/components/BreadCrumb'
 import IrbContact from '@/components/IrbContact'
 import Banner from '@/components/Banner/index'
-
-// Database // Schema
-import { connectMongoDB, disconnectMongoDB } from '@/service/db'
-import Page from '@/service/model/schemas/pageSchema'
-import { Menu } from '@/service/model/schemas/menuSchema'
-import { Template } from '@/service/model/schemas/templateSchema'
-import { Address } from '@/service/model/schemas/addressSchema'
-import { Products as ProductsDb } from '@/service/model/schemas/productsSchema'
-import { Collection } from '@/service/model/schemas/collectionsSchema'
-
-// Context Api
-import { useState } from 'react'
 import ServiceAddress from '@/components/ServiceAddress'
-import { insertMenuInTemplate, formatStrToUrl } from '@/utils/functions'
+
+// Others
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 export default function Contato({ content }) {
-  const [partner] = useState(content?.collection)
+  const router = useRouter()
+  const [fullUrl, setFullUrl] = useState('')
 
+  const [partner] = useState(content?.collection)
   const [metaTitle] = useState(partner?.metaTitle)
   const [metaDescription] = useState(partner?.metaDescription)
   const [banners] = useState(partner?.banners)
   const [title] = useState(partner?.title)
-  //   const [description] = useState(content?.page.contentDescription)
-  //   const [logoContact] = useState(content?.page.logoContact)
 
   let whatsappNumber =
     partner.info.phone.find((number) => number.label === 'Whatsapp') || null
@@ -54,13 +43,20 @@ export default function Contato({ content }) {
   if (!whatsappNumber) whatsappNumber = '11994412805'
   if (!phoneNumber) phoneNumber = '11994412805'
 
-  const categoryAndProduct = [...content.categories, ...content.products]
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const url = window.location.href
+      setFullUrl(url)
+    }
+  }, [router])
+
   return (
     <>
       <Head>
         <title>{metaTitle || title}</title>
-        <meta name="description" content={metaDescription || description} />
+        <meta name="description" content={metaDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href={fullUrl} />
       </Head>
       <Templates
         template={content?.template}
--- src/components/ServiceAddress/index.jsx
+++ src/components/ServiceAddress/index.jsx
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import SectionTitle from '../SectionTitle'
-import ProductsCard from '../Products/components/ProductsCard'
 import Address from '../Address'
 import { sortByKey } from '@/utils/functions'
 import CategoryGrid from '../CategoryGrid'
 
 export default function ServiceAddress({ products, address }) {
   const sortedCategories = sortByKey(products, 'label')
+
   return (
-    <section className="flex flex-col items-center" id={`service-address_`}>
+    <section id="service-address_" className="flex flex-col items-center">
       <div className="my-4 mb-14 flex w-full max-w-7xl flex-col md:mb-10 md:gap-10">
         <div className="flex flex-col gap-0 md:gap-10">
           <div className="w-12/12 px-6 md:px-14">
@@ -18,9 +17,7 @@ export default function ServiceAddress({ products, address }) {
             </div>
           </div>
           <div className="flex flex-1 flex-col">
-            {/* <SectionTitle title={'Serviço / Produtos'} className="md:mb-5"/> */}
             <div className="my-1 md:h-max">
-              {/* <ProductsCard products={products} limit={9} cards={products?.collection} textSize={'md:text-[1.2rem'}  heightCard={'!h-[95px]'}/> */}
               <CategoryGrid categories={sortedCategories} title />
             </div>
           </div>
