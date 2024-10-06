331a0ca8234ada3731ed2e7a8cd0826e80129ba5
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-10-06 13:42:04 -0300

    feat: add new layout to blog description screen

--- src/components/Pages/Singlepost/index.jsx
+++ src/components/Pages/Singlepost/index.jsx
@@ -1,7 +1,11 @@
 // Components
 import Templates from '@/components/Templates'
-import Banner from '@/components/Banner/index'
-import BreadCrumb from '@/components/BreadCrumb'
+import { BackgroundImageFirst } from '@/components/BackgroundImage/first'
+import { BackgroundImageLast } from '@/components/BackgroundImage/last'
+import Header from '@/components/Templates/Header'
+import Footer from '@/components/Templates/Footer'
+import Copyright from '@/components/Templates/Copyright'
+import { Title } from '@/pages/blog/components/Title'
 
 // Others
 import { useEffect, useState } from 'react'
@@ -14,8 +18,19 @@ export default function SinglePost({ content }) {
   const [fullUrl, setFullUrl] = useState('')
   const [sanitizedContent, setSanitizedContent] = useState('')
 
-  const { banners } = content?.page || {}
-  const { posts } = content || {}
+  const { posts, page } = content || {}
+  const {
+    components: { title: titleComponent },
+  } = page || {}
+
+  const arrHeader = content?.template?.find((item) => item?.label === 'header')
+  const header = arrHeader?.items.find(
+    (item) => item?.label === 'redesign-home',
+  )
+  const footer = content?.template?.find((item) => item?.label === 'footer')
+  const copyright = content?.template?.find(
+    (item) => item?.label === 'copyright',
+  )
 
   useEffect(() => {
     if (posts) {
@@ -30,6 +45,8 @@ export default function SinglePost({ content }) {
     }
   }, [router])
 
+  console.log()
+
   return (
     <>
       <Head>
@@ -42,23 +59,29 @@ export default function SinglePost({ content }) {
         page={content?.page}
         menus={content?.menus}
       >
-        <Banner banners={banners} />
-        <BreadCrumb />
-        <div className="max-w-7xl px-3.5 py-8 md:mx-auto">
-          <h1 className="mb-4 text-3xl font-bold">{posts?.title}</h1>
-          <p className="mb-4 text-gray-600">
-            {posts?._createdAt.split('T')[0]}
-          </p>
-          <div
-            className="custom-link-container prose overflow-hidden"
-            dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-          ></div>
-          <div className="mt-8">
-            <Link href="/blog" className="text-blue-500 hover:underline">
-              Voltar para Blog
-            </Link>
+        <BackgroundImageFirst backgrounds={content?.page?.backgroundImages}>
+          <Header content={header} page={content?.page?.label} />
+          <Title title={titleComponent} />
+        </BackgroundImageFirst>
+        <BackgroundImageLast backgrounds={content?.page?.backgroundImages}>
+          <div className="max-w-7xl px-5 py-12 md:mx-auto">
+            <h1 className="mb-4 text-3xl font-bold">{posts?.title}</h1>
+            <p className="mb-4 text-gray-600">
+              {posts?._createdAt.split('T')[0]}
+            </p>
+            <div
+              className="custom-link-container prose overflow-hidden"
+              dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+            ></div>
+            <div className="mt-8 w-fit cursor-pointer rounded-xl bg-[#22326E] px-5 py-2 transition-all duration-200 hover:scale-95">
+              <Link href="/blog" className="text-white">
+                Voltar para Blog
+              </Link>
+            </div>
           </div>
-        </div>
+          <Footer content={footer} />
+          <Copyright content={copyright} />
+        </BackgroundImageLast>
       </Templates>
     </>
   )
